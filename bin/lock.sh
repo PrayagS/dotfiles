3f93abc4373afe6f0c48b942560d5b1e10c10b76
PrayagS <prayag.s@ahduni.edu.in>
2020-06-18 13:03:05 +0530

    Font changes over the stack

--- bin/lock.sh
+++ bin/lock.sh
@@ -8,14 +8,14 @@
 # rm /tmp/screenshot.png
 
 xsecurelock_method(){
-   export XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 
-   export XSECURELOCK_FONT='JetBrainsMono Nerd Font'
-   export XSECURELOCK_SHOW_DATETIME=1 
-   export XSECURELOCK_SHOW_HOSTNAME=2 
-   export XSECURELOCK_SHOW_USERNAME=1 
+   export XSECURELOCK_DISCARD_FIRST_KEYPRESS=0
+   export XSECURELOCK_FONT='TerminessTTF Nerd Font'
+   export XSECURELOCK_SHOW_DATETIME=1
+   export XSECURELOCK_SHOW_HOSTNAME=2
+   export XSECURELOCK_SHOW_USERNAME=1
    export XSECURELOCK_LIST_VIDEOS_COMMAND='cat ~/.cache/wal/wal'
-   export XSECURELOCK_SAVER=saver_mpv 
-   xsecurelock 
+   export XSECURELOCK_SAVER=saver_mpv
+   xsecurelock
 }
 
 xsecurelock_method
