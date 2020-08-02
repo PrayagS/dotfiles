9ebe10143ef9de3a3067764e4d54027218130d4e
PrayagS <prayag.s@ahduni.edu.in>
2020-08-02 11:16:13 +0530

    Kill zcroll before restarting polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,6 +2,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
+killall zscroll
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
