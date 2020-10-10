fbf019f709dce2adea137e193d546ee32cf2bd94
PrayagS <prayag.s@ahduni.edu.in>
2020-10-10 18:12:09 +0530

    Update polybar config
    Redirect spotify bar stderr to null

--- config/polybar/scripts/get_spotify_status.sh
+++ config/polybar/scripts/get_spotify_status.sh
@@ -5,11 +5,11 @@ STATUS=$($HOME/.config/polybar/scripts/get_playerctl_status.sh)
 if [ "$STATUS" = "Stopped" ]; then
     echo "No music is playing"
 elif [ "$STATUS" = "Paused"  ]; then
-    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 >/dev/null
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 1>/dev/null 2>&1
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 elif [ "$STATUS" = "No player is running"  ]; then
     echo $STATUS
-else 
-    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 >/dev/null
+else
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 1>/dev/null 2>&1
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 fi
