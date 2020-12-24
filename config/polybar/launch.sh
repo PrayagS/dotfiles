e241fcf2c282a22630879ec63fd6e4eda64baaf0
PrayagS <prayag.s@ahduni.edu.in>
2020-12-24 22:21:14 +0530

    Update nvim and polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,11 +8,20 @@ killall zscroll
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars using the config location ~/.config/polybar/config.ini
-for bar in $HOME/.config/polybar/bars/*
-do
-  bar=${bar##*/}
-  bar=${bar%%.*}
-  polybar $bar -c /home/$USER/.config/polybar/config.ini &
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+TRAY_OUTPUT=eDP-1-1
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    for bar in "$HOME"/.config/polybar/bars/*
+    do
+        bar=${bar##*/}
+        bar=${bar%%.*}
+        if [[ "$bar" == "tray" ]]; then
+            echo "HERE"
+            MONITOR=$TRAY_OUTPUT polybar --reload "$bar" -c /home/"$USER"/.config/polybar/config.ini &
+        else
+            MONITOR=$m polybar --reload "$bar" -c /home/"$USER"/.config/polybar/config.ini &
+        fi
+    done
 done
 
 echo "Polybar launched..."
