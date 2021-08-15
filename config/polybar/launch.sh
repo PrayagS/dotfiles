4b1dd1a9211db12350f97fbc9ecec2328d0656e6
PrayagS <prayag.s@ahduni.edu.in>
2021-08-15 18:45:17 +0530

    Update polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,19 +8,18 @@ killall zscroll
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars using the config location ~/.config/polybar/config.ini
-MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
-TRAY_OUTPUT=eDP-1-1
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    for bar in "$HOME"/.config/polybar/bars/*
-    do
-        bar=${bar##*/}
-        bar=${bar%%.*}
-        if [[ "$bar" == "tray" ]]; then
-            MONITOR=$TRAY_OUTPUT polybar --reload "$bar" -c /home/"$USER"/.config/polybar/config.ini &
-        else
-            MONITOR=$m polybar --reload "$bar" -c /home/"$USER"/.config/polybar/config.ini &
-        fi
-    done
+CONFIG_LOCATION=/home/"$USER"/.config/polybar/config.ini
+for bar in "$HOME"/.config/polybar/bars/*
+do
+    bar=${bar##*/}
+    bar=${bar%%.*}
+    if [[ "$bar" == "i3-workspaces" || "$bar" == "polywins" ]]; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload "$bar" -c "$CONFIG_LOCATION" &
+        done
+    else
+        polybar --reload "$bar" -c "$CONFIG_LOCATION" &
+    fi
 done
 
 echo "Polybar launched..."
