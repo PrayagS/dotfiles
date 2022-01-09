ac9db6cd6b7d5361ae0dcfdab8ffd7c203b32cfb
PrayagS <prayag.s@ahduni.edu.in>
2022-01-09 19:27:32 +0530

    Make polybar launch script faster

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,23 +2,26 @@
 
 # Terminate already running bar instances
 killall -q polybar
-killall zscroll
+# killall zscroll
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars using the config location ~/.config/polybar/config.ini
+
 CONFIG_LOCATION=/home/"$USER"/.config/polybar/config.ini
+DISPLAY_PRIMARY=$(xrdb -query | grep 'display.primary'| awk '{print $NF}')
+DISPLAY_SECONDARY=$(xrdb -query | grep 'display.secondary'| awk '{print $NF}')
+
 for bar in "$HOME"/.config/polybar/bars/*
 do
     bar=${bar##*/}
     bar=${bar%%.*}
     if [[ "$bar" == "i3-workspaces" || "$bar" == "polywins" ]]; then
-        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-            MONITOR=$m polybar --reload "$bar" -c "$CONFIG_LOCATION" &
-        done
+        MONITOR=$DISPLAY_PRIMARY polybar --reload "$bar" -q -c "$CONFIG_LOCATION" &
+        MONITOR=$DISPLAY_SECONDARY polybar --reload "$bar" -q -c "$CONFIG_LOCATION" &
     else
-        polybar --reload "$bar" -c "$CONFIG_LOCATION" &
+        polybar --reload "$bar" -q -c "$CONFIG_LOCATION" &
     fi
 done
 
