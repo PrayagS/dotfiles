f6b5d0685702fbc40b4cb3342214c9885221e5b2
PrayagS <prayagsavsani@gmail.com>
2023-02-05 00:14:59 +0530

    Update aliases
    Change-Id: I3974d75b2cd9b8142e0338d19c0835155d1f044d
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,52 +1,52 @@
-# Shorthand aliases
-alias sudo="sudo "
-alias N='sudo -E n -e'
-alias ssctl="sudo systemctl"
-alias tlmgr="sudo tlmgr"
-alias cat="bat"
-alias less="/opt/homebrew/bin/less"
-alias yat="bat -l yaml"
-alias airshare="sudo airshare"
-alias mkdir="mkdir -pv"
-alias mv="mv -i"
-alias cp="cp -ir"
-alias ls="exa"
-alias l="exa -aghl --git --color=always --color-scale --group-directories-first"
-alias tcpdump="sudo tcpdump"
-alias k="kubectl"
-alias ks="kubectl --context=\"stage\""
-alias kt="kubectl --context=\"test\""
-alias vim="nvim"
-alias v="nvim"
-alias kssh="kitty +kitten ssh"
-alias oldbrew="/usr/local/bin/brew"
+# Shorthands
+alias -g sudo="sudo "
+alias -g N='sudo -E n -e'
+alias -g ssctl="sudo systemctl"
+alias -g tlmgr="sudo tlmgr"
+alias -g cat="bat"
+alias -g less="/opt/homebrew/bin/less"
+alias -g yat="bat -l yaml"
+alias -g airshare="sudo airshare"
+alias -g mkdir="mkdir -pv"
+alias -g mv="mv -i"
+alias -g cp="cp -ir"
+alias ls="exa --git --color=always --color-scale --group-directories-first"
+alias -g l="exa -aghl --git --color=always --color-scale --group-directories-first"
+alias -g tcpdump="sudo tcpdump"
+alias -g k="kubectl"
+alias -g ks="kubectl --context=\"stage\""
+alias -g kt="kubectl --context=\"test\""
+alias -g vim="nvim"
+alias -g v="nvim"
+alias -g kssh="kitty +kitten ssh"
+alias -g oldbrew="/usr/local/bin/brew"
 
-# Launch aliases
-alias filebot="java -jar /home/prayag_s/Downloads/misc-stuff/filebot/FileBot.jar"
-alias sxiv="sxiv -a"
-alias emu8086="WINEPREFIX=~/Games/osu\! wine Games/osu\!/drive_c/emu8086/emu8086.exe & disown && exit"
+# Launch
+alias -g filebot="java -jar /home/prayag_s/Downloads/misc-stuff/filebot/FileBot.jar"
+alias -g sxiv="sxiv -a"
+alias -g emu8086="WINEPREFIX=~/Games/osu\! wine Games/osu\!/drive_c/emu8086/emu8086.exe & disown && exit"
 
-# Pacman aliases
-alias pacrmcache="sudo pacman -Scc"
-alias yeet="yay -Rsn"
-alias fixmirrors="sudo pacman-mirrors --fasttrack && sudo pacman -Syyu"
+# Pacman
+alias -g pacrmcache="sudo pacman -Scc"
+alias -g yeet="yay -Rsn"
+alias -g fixmirrors="sudo pacman-mirrors --fasttrack && sudo pacman -Syyu"
 
 # Plex
-alias plexstart="sudo systemctl start plexmediaserver.service && /opt/google/chrome/google-chrome --profile-directory="Default" -a http://localhost:32400/web/index.html"
-alias plexstop="sudo systemctl stop plexmediaserver.service"
+alias -g plexstart="sudo systemctl start plexmediaserver.service && /opt/google/chrome/google-chrome --profile-directory="Default" -a http://localhost:32400/web/index.html"
+alias -g plexstop="sudo systemctl stop plexmediaserver.service"
 
 # VPN
-alias connectvpn="sudo systemctl start windscribe"
-alias disconnectvpn="windscribe disconnect && sudo systemctl stop windscribe"
+alias -g connectvpn="sudo systemctl start windscribe"
+alias -g disconnectvpn="windscribe disconnect && sudo systemctl stop windscribe"
 
 # PostgreSQL and PgAdmin
-alias startdb="sudo systemctl start postgresql.service && sudo python3 /usr/lib/pgadmin4/web/pgAdmin4.py"
-alias stopdb="sudo systemctl stop postgresql.service"
+alias -g startdb="sudo systemctl start postgresql.service && sudo python3 /usr/lib/pgadmin4/web/pgAdmin4.py"
+alias -g stopdb="sudo systemctl stop postgresql.service"
 
 # Misc
-alias findkeyname="xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'"
-alias rr="curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash"
+alias -g findkeyname="xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'"
+alias -g rr="curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash"
 
 # Work
-alias prod-sgp="export AWS_PROFILE=prod AWS_REGION=ap-southeast-1;kubectl ctx prod-sgp"
-alias stage-ore="export AWS_PROFILE=stage AWS_REGION=us-west-2;kubectl ctx stage-ore"
+alias -g prod-sgp="export AWS_PROFILE=prod AWS_REGION=ap-southeast-1;kubectl ctx prod-sgp"
+alias -g stage-ore="export AWS_PROFILE=stage AWS_REGION=us-west-2;kubectl ctx stage-ore"
