6c39e4737317bc570d57c48f0338a010627138cd
PrayagS <prayagsavsani@gmail.com>
2023-04-30 05:47:50 +0530

    Disable lazygit wrapper
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -16,19 +16,6 @@ pathadd() {
     fi
 }
 
-# lazygit wrapper
-lg()
-{
-    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
-
-    lazygit "$@"
-
-    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
-            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
-            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
-    fi
-}
-
 # nnn wrapper
 n ()
 {
