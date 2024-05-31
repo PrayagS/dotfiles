4efb58bad77b3afc6530c4301d111786cb42ad9d
PrayagS <prayagsavsani@gmail.com>
2024-06-01 03:29:05 +0530

    Update nvim-tree configuration
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/after/plugin/filetree.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-tree").setup({
-    tab = {
-        sync = {
-            open = true,
-        },
-    },
-})
-
-vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
--- config/nvim/lua/plugins/filetree.lua
+++ config/nvim/lua/plugins/filetree.lua
@@ -1,3 +1,32 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    config = function()
+        require("nvim-tree").setup({
+            hijack_cursor = true,
+            sync_root_with_cwd = true,
+            view = {
+                centralize_selection = true,
+            },
+            renderer = {
+                add_trailing = true,
+                group_empty = true,
+                indent_markers = {
+                    enable = true,
+                },
+            },
+            diagnostics = {
+                enable = true,
+            },
+            tab = {
+                sync = {
+                    open = true,
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+    end,
 }
