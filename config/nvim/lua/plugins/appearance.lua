d4fcd90a944715a18aabb8421fb7e9456db48253
PrayagS <prayagsavsani@gmail.com>
2024-06-01 01:26:57 +0530

    Switch primary theme to catppuccin
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -2,14 +2,14 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        -- priority = 1000, -- make sure to load this before all the other start plugins
     },
     {
         "catppuccin/nvim",
         name = "catppuccin",
-        -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        -- priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
     },
     "projekt0n/github-nvim-theme",
     "rebelot/kanagawa.nvim",
