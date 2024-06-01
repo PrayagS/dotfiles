6bebdadf7490a6f11f2ca58110529772525da013
PrayagS <prayagsavsani@gmail.com>
2024-06-02 00:24:29 +0530

    Lazy load treesitter
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,33 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c", "cpp", "cue",
+                "dockerfile",
+                "git_config", "gitcommit",
+                "go", "gomod", "gotmpl",
+                "json",
+                "lua",
+                "markdown",
+                "promql", "python",
+                "rust",
+                "terraform",
+                "yaml",
+                "vimdoc",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = { enable = true },
+        },
     },
-    "nvim-treesitter/nvim-treesitter-context"
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+        opts = {},
+    },
 }
