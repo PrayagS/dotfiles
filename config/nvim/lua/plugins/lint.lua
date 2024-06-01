438884968bfd20869db0dcfa1c5f390d30d49b2f
PrayagS <prayagsavsani@gmail.com>
2024-06-02 02:05:27 +0530

    Lazy load linter runner
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

---
+++ config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,18 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lint = require("lint")
+        lint.linters_by_ft = {
+            yaml = { "yamllint" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            group = lint_augroup,
+            callback = function()
+                require("lint").try_lint()
+            end,
+        })
+    end,
+}
