2ab022a342999e21885717bb47a1dd11a69f99e6
PrayagS <prayagsavsani@gmail.com>
2024-06-02 02:39:13 +0530

    Add formatter runner
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

---
+++ config/nvim/lua/plugins/format.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>ft",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+		-- Source: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#autoformat-with-extra-features
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+	},
+}
