3e1e94c220f675fc0f9b2bf070a6d3828fbbf174
PrayagS <prayagsavsani@gmail.com>
2024-06-02 04:24:42 +0530

    Preserve cursor position in autocmd
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -1,13 +1,19 @@
-vim.api.nvim_create_autocmd(
-    { "BufWritePre" },
-    {
-        group = vim.api.nvim_create_augroup("clean_whitespace", { clear = true }),
-        callback = function()
-            print("Hello world")
-            -- delete trailing whitespace
-            vim.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
-            -- delete trailing empty lines
-            vim.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
-        end,
-    }
-)
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = vim.api.nvim_create_augroup("clean_whitespace", { clear = true }),
+	callback = function()
+		-- save current cursor location
+		local cursor_position_before_save = vim.api.nvim_win_get_cursor(0)
+		local line, column = cursor_position_before_save[1], cursor_position_before_save[2]
+
+		-- delete trailing whitespace
+		vim.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
+		-- delete trailing empty lined
+		vim.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
+
+		-- restore cursor position
+		if line > vim.fn.line("$") then
+			line = vim.fn.line("$")
+		end
+		vim.api.nvim_win_set_cursor(0, { line, column })
+	end,
+})
