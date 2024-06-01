5d98adfeb7dda1a163b4aed7eb0ba2b67c3ea095
PrayagS <prayagsavsani@gmail.com>
2024-06-02 02:05:02 +0530

    Load completion for command line as well
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
     "hrsh7th/nvim-cmp",
     version = false,
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
