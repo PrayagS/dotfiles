7bc45c3f90dd81f27057c933ac51f7fd01efefc8
PrayagS <prayagsavsani@gmail.com>
2024-06-01 18:58:51 +0530

    Lazy load all git plugins
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,7 +1,36 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    "linrongbin16/gitlinker.nvim",
-    "sindrets/diffview.nvim",
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            current_line_blame = true,
+            current_line_blame_formatter_opts = {
+                relative_time = true,
+            },
+        },
+    },
+    {
+        "linrongbin16/gitlinker.nvim",
+        opts = {},
+        keys = {
+            { "<leader>gL", "<cmd>lua require('gitlinker').link({ action = require('gitlinker.actions').system })<cr>", mode = { "n", "v"}, desc = "GitLink!" },
+            { "<leader>gB", "<cmd>lua require('gitlinker').link({ router_type = 'blame', action = require('gitlinker.actions').system })<cr>", mode = { "n", "v"}, desc = "GitLink! blame" },
+            { "<leader>gD", "<cmd>lua require('gitlinker').link({ router_type = 'default_branch', action = require('gitlinker.actions').system })<cr>", mode = { "n", "v"}, desc = "GitLink! default_branch" },
+            { "<leader>gC", "<cmd>lua require('gitlinker').link({ router_type = 'current_branch', action = require('gitlinker.actions').system })<cr>", mode = { "n", "v"}, desc = "GitLink! current_branch" },
+        },
+    },
+    {
+        "sindrets/diffview.nvim",
+        opts = {},
+        cmd = {
+            "DiffviewOpen",
+            "DiffviewClose",
+            "DiffviewToggleFiles",
+            "DiffviewFocusFiles",
+            "DiffviewRefresh",
+            "DiffviewFileHistory",
+            "DiffviewLog",
+        },
+    },
     {
         "kdheepak/lazygit.nvim",
         cmd = {
@@ -10,7 +39,7 @@ return {
             "LazyGitCurrentFile",
             "LazyGitFilter",
             "LazyGitFilterCurrentFile",
-          },
+        },
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
