129581ce566155507e9ef47bdf2451aa736c3efd
PrayagS <prayagsavsani@gmail.com>
2024-07-28 14:34:34 +0530

    Update formatter preference in conform.nvim
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/format.lua
+++ config/nvim/lua/plugins/format.lua
@@ -14,15 +14,10 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-            go = { "golines" },
+			go = { "golines" },
 		},
-		-- Source: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#autoformat-with-extra-features
-		format_on_save = function(bufnr)
-			-- Disable with a global or buffer-local variable
-			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-				return
-			end
-			return { lsp_fallback = true }
-		end,
+		format_on_save = {
+			lsp_format = "first",
+		},
 	},
 }
--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -1,19 +1,30 @@
 return {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lint = require("lint")
-        lint.linters_by_ft = {
-            yaml = { "yamllint" },
-            go = { "golangcilint" },
-        }
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-            group = lint_augroup,
-            callback = function()
-                require("lint").try_lint()
-            end,
-        })
-    end,
+		lint.linters.jsonnet_lint = {
+			cmd = "jsonnet-lint",
+			stdin = true,
+			args = { "-" },
+			stream = "stdout",
+			ignore_exitcode = true,
+			parser = require("lint.parser").from_errorformat("%f:%l:%c-%k %m"),
+		}
+
+		lint.linters_by_ft = {
+			jsonnet = { "jsonnet_lint" },
+			go = { "golangcilint" },
+			yaml = { "yamllint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
 }
