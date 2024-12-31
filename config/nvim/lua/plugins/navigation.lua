4d7cfa10799005fa2609e098cabbe0cb3ba00f41
PrayagS <prayagsavsani@gmail.com>
2025-01-01 01:04:38 +0530

    leap.nvim -> flash.nvim
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"ggandor/leap.nvim",
+		enabled = false,
 		lazy = false,
 		opts = {},
 		config = function()
@@ -33,6 +34,7 @@ return {
 	},
 	{
 		"ggandor/flit.nvim",
+		enabled = false,
 		dependencies = {
 			"ggandor/leap.nvim",
 		},
@@ -72,13 +74,56 @@ return {
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 		config = function()
 			require("flash").setup({
 				label = {
 					rainbow = { enabled = true },
 				},
 				modes = {
-					char = { enabled = false },
+					char = { enabled = true },
+					search = { enabled = true },
 					treesitter = {
 						label = {
 							style = "inline",
