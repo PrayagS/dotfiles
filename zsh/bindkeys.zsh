16f53cf143a4d459df5da3778f6923ec1dc08967
PrayagS <prayagsavsani@gmail.com>
2025-01-01 22:45:34 +0530

    Go back to block cursors in the shell
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -30,22 +30,22 @@ bindkey -M menuselect 'j' vi-down-line-or-history
 function zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]] ||
      [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
+    echo -ne '\e[2 q'
   elif [[ ${KEYMAP} == main ]] ||
        [[ ${KEYMAP} == viins ]] ||
        [[ ${KEYMAP} = '' ]] ||
        [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
+    echo -ne '\e[1 q'
   fi
 }
 zle -N zle-keymap-select
 zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
+    echo -ne "\e[1 q"
 }
 zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+echo -ne '\e[1 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[1 q' ;} # Use beam shape cursor for each new prompt.
 
 # ci", ci', ci`, di", etc
 autoload -U select-quoted
