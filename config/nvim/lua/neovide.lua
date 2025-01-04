38aca66eb0349571a415ef21c3d5a8ceef3ebe00
PrayagS <prayagsavsani@gmail.com>
2025-01-04 22:10:35 +0530

    Add neovide for opening files from Finder
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

---
+++ config/nvim/lua/neovide.lua
@@ -0,0 +1,12 @@
+if vim.g.neovide then
+	vim.g.neovide_transparency = 0.75
+	vim.g.neovide_window_blurred = true
+
+	vim.g.neovide_refresh_rate = 120
+
+	vim.g.neovide_cursor_animation_length = 0.1
+	vim.g.neovide_cursor_trail_size = 0.1
+	vim.g.neovide_cursor_smooth_blink = true
+
+	vim.o.guifont = "Maple Mono:h14"
+end
