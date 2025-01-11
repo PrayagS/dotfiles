1b7a98ff803d25bdc42e774cc7e132ca24e877e9
PrayagS <prayagsavsani@gmail.com>
2025-01-11 21:14:43 +0530

    nvim: add autopairs rule
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -10,6 +10,26 @@ return {
 				map_bs = false,
 				disable_in_visualblock = true,
 			})
+			-- Ref: https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#insertion-with-surrounding-check
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			function Insertion_with_surrounding_check(a1, ins, a2, lang)
+				npairs.add_rule(Rule(ins, ins, lang)
+					:with_pair(function(opts)
+						return a1 .. a2 == opts.line:sub(opts.col - #a1, opts.col + #a2 - 1)
+					end)
+					:with_move(cond.none())
+					:with_cr(cond.none())
+					:with_del(function(opts)
+						local col = vim.api.nvim_win_get_cursor(0)[2]
+						return a1 .. ins .. ins .. a2 == opts.line:sub(col - #a1 - #ins + 1, col + #ins + #a2) -- insert only works for #ins == 1 anyway
+					end))
+			end
+
+			Insertion_with_surrounding_check("{", " ", "}")
+			Insertion_with_surrounding_check("(", " ", ")")
+			Insertion_with_surrounding_check("[", " ", "]")
 		end,
 	},
 	{
