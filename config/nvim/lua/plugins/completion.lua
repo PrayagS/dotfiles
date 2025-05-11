1e241a681b8ea189149ec2cb96869af4bc78cdd8
PrayagS <prayagsavsani@gmail.com>
2025-05-11 16:57:17 +0530

    feat(nvim/completion): make completion menu and ghost text work together
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -130,6 +130,27 @@ return {
 						columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
 					},
 					border = "bold",
+					-- Ref: https://main.cmp.saghen.dev/recipes#avoid-multi-line-completion-ghost-text
+					direction_priority = function()
+						local ctx = require("blink.cmp").get_context()
+						local item = require("blink.cmp").get_selected_item()
+						if ctx == nil or item == nil then
+							return { "s", "n" }
+						end
+
+						local item_text = item.textEdit ~= nil and item.textEdit.newText
+							or item.insertText
+							or item.label
+						local is_multi_line = item_text:find("\n") ~= nil
+
+						-- after showing the menu upwards, we want to maintain that direction
+						-- until we re-open the menu, so store the context id in a global variable
+						if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+							vim.g.blink_cmp_upwards_ctx_id = ctx.id
+							return { "n", "s" }
+						end
+						return { "s", "n" }
+					end,
 				},
 				documentation = {
 					auto_show = true,
