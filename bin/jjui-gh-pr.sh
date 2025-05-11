2eeebcd98d2bda6269047443e693f23ad774e66e
PrayagS <prayagsavsani@gmail.com>
2025-05-11 16:57:17 +0530

    fix(bin/jjui-gh-pr): choose the first bookmark
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

--- bin/jjui-gh-pr.sh
+++ bin/jjui-gh-pr.sh
@@ -1,5 +1,5 @@
 CHANGE_ID="$1"
 
-BOOKMARK=$(jj log --no-graph -T "self.bookmarks()" -r "$CHANGE_ID")
+BOOKMARK=$(jj log --no-graph -T "self.bookmarks()" -r "$CHANGE_ID" | choose 0)
 
 gh pr create --fill-verbose --head ${BOOKMARK} | pbcopy
